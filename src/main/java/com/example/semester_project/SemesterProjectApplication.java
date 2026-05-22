c0bae600861c533a2123b599ac8efad691bceca7
olesherka <smoke.paladin@gmail.com>
2026-05-22 18:32:57 +0500

    first commit

---
+++ src/main/java/com/example/semester_project/SemesterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.semester_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemesterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemesterProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/semester_project/SemesterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semester_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemesterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
